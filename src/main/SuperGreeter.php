254ab35251343e5a4b7aeeec4e32167da4fece31
Wouter Admiraal <wad@wadmiraal.net>
2025-01-21 09:58:05 +0100

    Initial commit

---
+++ src/main/SuperGreeter.php
@@ -0,0 +1,14 @@
+<?php
+namespace App;
+
+class Greeter {
+  public function greet(string $name = null): string {
+    if ($name) {
+      return "Hello, $name!";
+    }
+
+    setcookie("TestCookie", $value, time()+3600, "/~path/", ".com", 1); // Noncompliant
+
+    return "Hello!";
+  }
+}
